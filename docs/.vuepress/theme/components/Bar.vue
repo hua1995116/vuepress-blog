3e589be08e31d724d3f39f50d28aadc3032f78c5
huayifeng <461249104@qq.com>
2021-03-04 01:16:29 +0800

    fix: tengxun

--- docs/.vuepress/theme/components/Bar.vue
+++ docs/.vuepress/theme/components/Bar.vue
@@ -4,6 +4,7 @@
       <div class="text">
         <p>阿里云优惠活动，点击链接进行购买: <a href="https://www.aliyun.com/minisite/goods?userCode=qtt5ztpk">一年仅需96.9元即可以购买服务器~ </a></p>
         <p>腾讯云优惠活动, 点击链接进行购买<a href="https://curl.qcloud.com/mcBHz0hb">一年仅需99元</a></p>
+        <p>腾讯云限时开团活动, 点击链接进行购买<a href="https://cloud.tencent.com/act/cvmgift?hash=K468jiO7GIO6QaYN">一年仅需95元</a></p>
         <p><a href="/op/service-choice.html">各大服务器厂商对比选购</a></p>
       </div>
     </div>
